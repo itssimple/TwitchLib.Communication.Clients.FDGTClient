1e6e60778de21ba2380a0a03646dd8fff95fdb99
NoLifeKing <webmaster@itssimple.se>
2021-01-20 07:50:01 +0100

    Add project files.

---
+++ TwitchLib.Communication.Clients.FDGTClient/FDGTClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace TwitchLib.Communication.Clients
+{
+    public class FDGTClient : WebSocketClient
+    {
+
+        public FDGTClient() : base()
+        {
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var urlField = base.GetType().BaseType.GetFields(bindingFlags).FirstOrDefault(f => f.Name.Contains("Url"));
+            urlField.SetValue(this, "wss://irc.fdgt.dev:443");
+        }
+    }
+}
